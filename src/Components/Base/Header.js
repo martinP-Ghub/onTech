025a987ec8acb4ebf27fc7d35ce629abd49dc5ac
martin <inb7902@naver.com>
2023-05-17 22:25:24 +0900

    header, footer css modification

--- src/Components/Base/Header.js
+++ src/Components/Base/Header.js
@@ -3,13 +3,14 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="main-menu">
+            <div>logo</div>
             <nav>
                 <ul>
-                    <li><a href="#home">HOME</a></li>
-                    <li><a href="#about">ABOUT</a></li>
-                    <li><a href="#service">SERVICE</a></li>
-                    <li><a href="#request">REQUEST</a></li>
-                    <li><a href="#contact">CONTACT</a></li>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">SERVICE</a></li>
+                    <li><a href="#">REQUEST</a></li>
+                    <li><a href="#">CONTACT</a></li>
                 </ul>
             </nav>
         </header>
